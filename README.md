df33d6c7e8fb612e281d833cae092ac2f10e1047
Michael Esposito <michaelde1984@gmail.com>
2015-12-11 20:55:55 -0500

    Update 2015-12-12T01:54:23.390Z

--- README.md
+++
@@ -1,1 +0,0 @@
-This is a simple chat application I built using backbone in week 7 at The Iron Yard.  Once you type a username and message, hit submit and refresh the page to see your message appear.
--- app/scripts/models/messages-collection.js
+++
@@ -1,8 +0,0 @@
-import Message from 'models/message';
-
-var MessageCollection = Backbone.Collection.extend({
-  model: Message,
-  url: "http://tiny-lasagna-server.herokuapp.com/collections/messages"
-});
-
-export default MessageCollection;
